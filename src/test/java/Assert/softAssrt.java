36d57cd58d808dd8e5b00d0b3c4007865b899ed9
RahulGuptaTFT <g.rahul@tftus.com>
2023-12-12 17:26:08 +0530

    last

--- src/test/java/Assert/softAssrt.java
+++ src/test/java/Assert/softAssrt.java
@@ -7,7 +7,7 @@ import org.testng.asserts.SoftAssert;
 public class softAssrt {
     @Test
     void print(){
-        SoftAssert soft  =new SoftAssert();
+        SoftAssert soft = new SoftAssert();
         System.out.println("start");
         soft.assertEquals(1,2,"Not match");
         System.out.println("complete");
--- src/test/java/TestNGBasics/exceptionTimeoutTest.java
+++ src/test/java/TestNGBasics/exceptionTimeoutTest.java
@@ -5,14 +5,15 @@ import org.testng.annotations.Test;
 public class exceptionTimeoutTest {
 
     @Test(timeOut = 1)
-    void print (){
-        int i=0;
-        while (i==0){
+    void print() {
+        int i = 0;
+        while (i == 0) {
             System.out.println(i);
         }
     }
+
     @Test(expectedExceptions = NumberFormatException.class)
-    void test(){
+    void test() {
         String i = "123";
         Integer.parseInt("");
     }
